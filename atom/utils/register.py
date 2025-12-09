2cecf448dbd37184cf6b8dd41c9b8a453c2901aa
zejunchen-zejun <zejun.chen@amd.com>
2025-12-09 16:29:45 +0800

    add model
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- atom/utils/register.py
+++ atom/utils/register.py
@@ -4,9 +4,11 @@ try:
     from vllm import ModelRegistry
 
     # Register models as plugin to vllm
+    print('[zejun][atom] Registering ATOM model plugin to vllm', flush=True)
     ModelRegistry.register_model(
         "Qwen3ForCausalLM",
         "atom.models.qwen3:Qwen3ForCausalLM")
+    print('[zejun][atom] Finish Registering ATOM model plugin to vllm', flush=True)
 
 except ImportError:
     warnings.warn("vllm is not installed. Bypass the model registration to vllm", ImportWarning)
--- test_plugin/test.sh
+++ test_plugin/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
 export VLLM_ROCM_USE_ATOM_PLUGIN=1
 
 export SAFETENSORS_FAST_GPU=1
@@ -13,17 +12,14 @@ export TORCHINDUCTOR_CACHE_DIR=/root/.cache/inductor
 
 rm -rf /root/.cache/
 
-model_path=/raid/data/model/Qwen3-235B-A22B-Instruct-2507-FP8
+model_path=/raid/data/model/Qwen3-0.6B
 
 vllm serve $model_path \
     --host localhost \
     --port 9090 \
-    --tensor-parallel-size 4 \
-    --enable-expert-parallel \
-    --max-num-batched-tokens 20000 \
+    --tensor-parallel-size 1 \
     --kv-cache-dtype "fp8" \
     --trust-remote-code \
-    --no-enable-prefix-caching \
     --disable-log-requests \
     --gpu_memory_utilization 0.6 \
     --enforce-eager \
