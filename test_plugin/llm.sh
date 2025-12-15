9fbb37c1474d53bcfa9195b295a2113608f2cf6a
zejunchen-zejun <zejun.chen@amd.com>
2025-12-15 16:35:39 +0800

    add
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

---
+++ test_plugin/llm.sh
@@ -0,0 +1,19 @@
+addr=localhost
+port=9090
+url=http://${addr}:${port}/v1/completions
+model=/raid/data/model/Qwen3-235B-A22B-Instruct-2507-FP8
+bs=1
+task=gsm8k
+
+echo "url=${url}"
+echo "model=${model}"
+echo "task=${task}"
+
+lm_eval \
+    --model local-completions \
+    --tasks ${task} \
+    --model_args model=${model},base_url=${url} \
+    --batch_size ${bs} \
+    --seed 123 \
+    --limit 10 \
+    2>&1 | tee log.lmeval.log
