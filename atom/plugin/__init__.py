8eecde9d2d8ddc1ec4032138f80f520225cc5396
zejunchen-zejun <zejun.chen@amd.com>
2025-12-30 18:56:18 +0800

    add
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- atom/plugin/__init__.py
+++ atom/plugin/__init__.py
@@ -1,3 +1,6 @@
-from .prepare import prepare_model
-from .prepare import is_sglang, is_vllm
-from .prepare import is_plugin_mode
+from .prepare import (
+    prepare_model,
+    is_sglang,
+    is_vllm,
+    is_plugin_mode,
+)
