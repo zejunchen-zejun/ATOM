2f7130649786c241c3494e7d4449bb8d18b34dbc
gbyu-amd <Guanbao.Yu@amd.com>
2026-01-06 10:18:17 +0800

    [CI] add Qwen3-235B-A22B-Instruct-2507-FP8 to CI (#110)
    * add Qwen3-235B-A22B-Instruct-2507-FP8 to CI
    
    * fix per-token kv scale for gluon pa decode
    
    ---------
    
    Co-authored-by: Guanbao Yu <gyu@amd.com>

--- atom/model_ops/attention_mha.py
+++ atom/model_ops/attention_mha.py
@@ -285,8 +285,10 @@ class Attention(nn.Module):
             context_partition_size,
             tl.bfloat16, #compute_type
             None,
-            self.kv_scale,
-            self.kv_scale,
+            # when using per-token quant, original k_scale shape: [num_blocks, block_size, num_kv_heads]
+            # gluon pa decode kernel expects shape: [num_blocks, num_kv_heads, block_size, 1]
+            self.kv_scale if self.sinks is not None else k_scale.unsqueeze(-1).transpose(1, 2),
+            self.kv_scale if self.sinks is not None else v_scale.unsqueeze(-1).transpose(1, 2),
             exp_sums=exp_sums,
             max_logits=max_logits,
             temporary_output=temporary_output,
